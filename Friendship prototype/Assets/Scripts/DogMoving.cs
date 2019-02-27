978d52eac618000e5833a6308796d41111e3cb46
Jayne <jaynegale@gmail.com>
2019-02-27 15:30:42 +1300

    Audio Zones set up

---
+++ Friendship prototype/Assets/Scripts/DogMoving.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogMoving : MonoBehaviour
+{
+
+    public AudioSource thisDogBark;
+    public AudioSource nextDogBark;
+    public AudioSource nextAudio;
+    AudioSource thisAudio;
+
+    void Start () {
+        thisAudio = GetComponent<AudioSource>();
+	}
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            thisAudio.Stop();
+            thisDogBark.Stop();
+  //          nextAudio.Play();
+   //         nextDogBark.Play();
+        }
+    }
+ }
