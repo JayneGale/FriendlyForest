7fb97fde31de26cc17218d1783a9f69be68fb333
Jayne Gale <jaynegale@gmail.com>
2019-03-03 23:42:19 +1300

    GetKeyDown script stopped dogs barking

--- Friendship prototype/Assets/Scripts/DogMoving.cs
+++ Friendship prototype/Assets/Scripts/DogMoving.cs
@@ -8,8 +8,9 @@ public class DogMoving : MonoBehaviour
     public GameObject nextDog;
     AudioSource nextDogBark;
     AudioSource forestAmbient;
-  //  public AudioClip[] dogBarks;
+    public AudioClip[] dogBarks;
     Animator nextDogAnimator;
+    AudioClip randomisedClip;
 
     private void Start()
     {
@@ -20,29 +21,34 @@ public class DogMoving : MonoBehaviour
         nextDogBark = nextDog.GetComponent<AudioSource>();
         forestAmbient = gameObject.GetComponent<AudioSource>();
         nextDogAnimator = nextDog.GetComponent<Animator>();
-  /*      if (dogBarks.Length < 1)
+        if (dogBarks.Length < 1)
         {
             Debug.Log("No audio clips assigned to " + gameObject.name);
             Application.Quit();
         }
-  */
+
+    }
+
+    private void Update()
+    { 
+        if (Input.GetKeyDown("E"))
+        {
+            nextDogBark.Play();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-  /*          {
-                int barkSelected = Random.Range(0, dogBarks.Length);
-                nextDogBark.PlayOneShot(dogBarks[barkSelected]);
-//I don't want it to play one shot I want it to loop
-                }
-*/
-            nextDogBark.Play();
             forestAmbient.enabled = true;
             forestAmbient.Play();
-//            }
+            int barkSelected = Random.Range(0, dogBarks.Length);
+            randomisedClip = dogBarks[barkSelected];
             nextDogAnimator.enabled = true;
+            //           nextDogBark.Play();
+            //           nextDogBark.PlayOneShot(dogBarks[barkSelected]);
+            //I don't want it to play one shot I want it to loop
         }
     }
 
