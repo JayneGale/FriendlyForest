d89ac2b505b9687e38d5ac3b657f279e60a83442
Jayne <jaynegale@gmail.com>
2019-03-07 15:18:44 +1300

    Various AI trials

---
+++ Friendship prototype/Assets/Scripts/DogWayfinder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DogWayfinder : MonoBehaviour {
+
+    [SerializeField]
+    Transform nextWaypoint;
+
+ //   Transform waypointAfterThat;
+    NavMeshAgent navmeshAgent;
+
+
+	// Use this for initialization
+	void Start () {
+        navmeshAgent = this.GetComponent<NavMeshAgent>();
+        if (navmeshAgent == null)
+        {
+            Debug.LogError("NavmeshAgent component is not attached to " + gameObject.name);                
+        }
+        else
+        {
+            SetDestination();
+        }
+             
+		
+	}
+
+    private void SetDestination()
+    {
+        if (nextWaypoint != null)
+        {
+            Vector2 targetVector = nextWaypoint.transform.position;
+            float zpos = nextWaypoint.transform.position.z;
+            Debug.Log("Bone z position is " + zpos);
+            navmeshAgent.SetDestination(targetVector);
+        }
+    }
+
+  }
