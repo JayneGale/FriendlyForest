ed89b21df6c1fd6edb54e4d028b62bbf983adf63
Jayne <jaynegale@gmail.com>
2019-03-08 15:21:41 +1300

    Start making player zones for dog

--- Friendship prototype/Assets/Scripts/DogWayfinder.cs
+++ Friendship prototype/Assets/Scripts/DogWayfinder.cs
@@ -6,28 +6,38 @@ using UnityEngine.AI;
 
 public class DogWayfinder : MonoBehaviour {
 
-   // [SerializeField]
-   // Transform nextWaypoint;
+    // [SerializeField]
+    // Transform nextWaypoint;
 
+    [SerializeField]
+    int zonePlayerIsIn;
+    //    bool dogMarkingPlace;
+
+    [SerializeField]
+    int nextRightZone;
+   // float totalWaitTime = 3.0f;
     [SerializeField]
     List<DrawGizmo> dogWayPoints;
 
+    //is the dog moving towards its destination
     bool dogMoving;
-  //  bool dogWaiting;
+    //is the dog waiting at the current destination
     bool waiting;
-    float waitTime;
-    public float totalWaitTime;
+    // do we want the dog waiting at all - could get rid of this
+    bool markingPlace;
+
+    float waitTimer;
     public int startWayPointIndex;
     int currentWayPointIndex;
+    Transform player;
 
     NavMeshAgent navmeshAgent;
 
-
-	// Use this for initialization
 	void Start ()
     {
         navmeshAgent = this.GetComponent<NavMeshAgent>();
         Debug.Log(" dogWayPoints.Count is " + dogWayPoints.Count);
+  //      player = GameObject.FindGameObjectWithTag("Player").transform;
 
         if (navmeshAgent == null)
         {
@@ -35,51 +45,63 @@ public class DogWayfinder : MonoBehaviour {
         }
         else
         {
-            if (dogWayPoints != null && dogWayPoints.Count >= 2)
-            {
+//            if (dogWayPoints != null && dogWayPoints.Count >= 2)
+            if (dogWayPoints != null && dogWayPoints.Count >= 1)
+                {
                 currentWayPointIndex = startWayPointIndex;
                 SetDestination();
             }
             else
             {
-                Debug.LogError("Insufficient Waypoints attached to " + gameObject.name);
+                Debug.LogError("No Waypoint attached to " + gameObject.name);
             }
         }
-             		
 	}
 
     public void Update()
     {
-        if(dogMoving && navmeshAgent.remainingDistance <= 1.0f)
+        if (dogMoving && navmeshAgent.remainingDistance <= 1.0f)
         {
             dogMoving = false;
-            if (waiting)
+        }
+            ChangeWaypoint();
+            SetDestination();
+            /*            if (markingPlace)
+                        {
+                            waiting = true;
+                            waitTimer = 0.0f;
+                        }
+                        else
+                        {
+                            ChangeWaypoint();
+                            SetDestination();
+                        }
+           */
+        }
+/*
+        if (waiting)
+        {
+            waitTimer += Time.deltaTime;
+
+            if (waitTimer >= totalWaitTime)
             {
-                waiting = true;
-                waitTime = 0.0f;
-            }
-            else
-            {
+                waiting = false;
                 ChangeWaypoint();
                 SetDestination();
-            }
-            if (waiting)
-            {
-                waitTime += Time.deltaTime;
-                if(waitTime >= totalWaitTime)
-                {
-                    waiting = false;
-                    ChangeWaypoint();
-                    SetDestination();
-                }
             }
         }
     
     }
-
+ */
+     
     private void ChangeWaypoint()
     {
-        currentWayPointIndex = (currentWayPointIndex + 1) % dogWayPoints.Count;
+        currentWayPointIndex = (currentWayPointIndex + 1);
+        if (currentWayPointIndex >= dogWayPoints.Count)
+        {
+            currentWayPointIndex = dogWayPoints.Count-1;
+        }
+ //           currentWayPointIndex = (currentWayPointIndex + 1) % dogWayPoints.Count;
     }
 
     private void SetDestination()
